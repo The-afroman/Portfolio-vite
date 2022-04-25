0af3132da09b1b4695e43968e9e60eb50f19b8eb
Faris <fhijazi00@gmail.com>
2022-04-25 03:55:03 -0700

    fixed public paths

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -39,28 +39,28 @@ class App extends React.Component {
         <h1>Projects :</h1>
         <div className='Cards'>
           <div className='col'>
-            <Card name="HTML proxy server" description="HTML 1.1 compatible proxy server in python written for a computer networking class" imgsrc="webproxy.png" link="https://github.com/The-afroman/ecs152a-proxy-server" />
+            <Card name="HTML proxy server" description="HTML 1.1 compatible proxy server in python written for a computer networking class" imgsrc={process.env.PUBLIC_URL+"/webproxy.png"} link="https://github.com/The-afroman/ecs152a-proxy-server" />
 
 
 
-            <Card name="Fitness tracking website" description="activity tracking website with mobile and desktop views with Google Oauth user authentication and login using HTML, CSS, Javascript, SQlite and Node/express for server and backend. Created for web design class." imgsrc="fitness_tracker.png" link="https://github.com/The-afroman/Fitness-Tracker" />
+            <Card name="Fitness tracking website" description="activity tracking website with mobile and desktop views with Google Oauth user authentication and login using HTML, CSS, Javascript, SQlite and Node/express for server and backend. Created for web design class." imgsrc={process.env.PUBLIC_URL+"/fitness_tracker.png"} link="https://github.com/The-afroman/Fitness-Tracker" />
           </div>
 
           <div className='col'>
-            <Card name="WebGL 3D viewer" description="WebGL 3D viewing program, uses custom written glsl shaders for phong and gouraud lighting models with bump mapping for normals. Ability to interactively change lighting position color and diffuse and specular attributes of individual point or directional lights. Custom scene graph data structure to represent the scene. Final project for computer graphics class." imgsrc="webgl.png" link="https://github.com/The-afroman/computer-graphics-3D-viewer" />
+            <Card name="WebGL 3D viewer" description="WebGL 3D viewing program, uses custom written glsl shaders for phong and gouraud lighting models with bump mapping for normals. Ability to interactively change lighting position color and diffuse and specular attributes of individual point or directional lights. Custom scene graph data structure to represent the scene. Final project for computer graphics class." imgsrc={process.env.PUBLIC_URL+"/webgl.png"} link="https://github.com/The-afroman/computer-graphics-3D-viewer" />
 
-            <Card name="Personal Site" description="portfolio showcase (this site) created with react, express, node, typescript" imgsrc="portfolio.png" link="https://github.com/The-afroman/Portfolio" />
+            <Card name="Personal Site" description="portfolio showcase (this site) created with react, express, node, typescript" imgsrc={process.env.PUBLIC_URL + "/portfolio.png"} link="https://github.com/The-afroman/Portfolio" />
 
           </div>
 
           <div className='col'>
-            <Card name="UC Davis interactive webapp" description="A web app to visualize UC Davis revenue and expenses with charts and animations using HTML, Javascript, CSS, React and D3. Created as the final project for a web design class" imgsrc="slices.png" link="https://github.com/The-afroman/ecs162-final-proj" />
+            <Card name="UC Davis interactive webapp" description="A web app to visualize UC Davis revenue and expenses with charts and animations using HTML, Javascript, CSS, React and D3. Created as the final project for a web design class" imgsrc={process.env.PUBLIC_URL+"/slices.png"} link="https://github.com/The-afroman/ecs162-final-proj" />
 
           </div>
         </div>
         <h1>Resume :</h1>
         <div className="Resume">
-          <object data="Resume.pdf" width="800" height="2000" />
+          <embed src={process.env.PUBLIC_URL+"/Resume.pdf"} width="800" height="2000" />
         </div>
 
       </div>
